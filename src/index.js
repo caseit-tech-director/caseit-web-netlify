baa940fffea303ef101d68369c95265ff93e18a4
Alvin <alvinleung2009@gmail.com>
2020-07-24 03:36:24 -0700

    Improved Navigation
    - auto hide after scroll
    - drop shadow after scroll

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ console.log("🦊 Hello! Edit me in src/index.js");
 // Hamburger menu module
 window.addEventListener("load", (e) => {
   setupHamburgerMenu();
+  setupMenuHideOnScroll();
+  setupMenuScrolledLook();
 });
 
 function setupHamburgerMenu() {
@@ -26,6 +28,52 @@ function setupHamburgerMenu() {
       // hamburger menu is collapsed
       hamburgerMenuToggle.classList.add("hamburger-menu-toggle--cross");
       // navMenu.classList.add("nav-main--expanded");
+    }
+  });
+}
+
+
+// adapted from
+// https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp
+function setupMenuHideOnScroll() {
+  let prevScrollpos = window.pageYOffset;
+  let minScrolPos = 100; // when does it start shrinking
+
+  let hamburgerMenuToggle = document.querySelector(".hamburger-menu-toggle");
+  let navBar = document.querySelector("nav");
+
+  window.addEventListener("scroll", (e) => {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos < currentScrollPos && currentScrollPos > minScrolPos) {
+      // hide nav bar here
+      if (
+        !navBar.classList.contains("hidden") &&
+        !hamburgerMenuToggle.classList.contains("hamburger-menu-toggle--cross")
+      )
+        navBar.classList.add("hidden");
+    } else {
+      // show nav bar here
+      if (navBar.classList.contains("hidden"))
+        navBar.classList.remove("hidden");
+    }
+    prevScrollpos = currentScrollPos;
+  });
+}
+
+function setupMenuScrolledLook() {
+  let scrolled = false;
+  let navBar = document.querySelector("nav");
+
+  window.addEventListener("scroll", (e) => {
+    let currentScrollPos = window.pageYOffset;
+
+    // setup "scrolled" look when back is not zero
+    if (currentScrollPos != 0 && !scrolled) {
+      navBar.classList.add("scrolled");
+      scrolled = true
+    } else if (currentScrollPos == 0) {
+      scrolled = false;
+      navBar.classList.remove("scrolled");
     }
   });
 }

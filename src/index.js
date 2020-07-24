d328116ec70f9f3b452486dbef43b09a6dfdd614
Alvin <alvinleung2009@gmail.com>
2020-07-23 22:58:25 -0700

    Setup basic navigation module
    The navigation now responsive to different when it shrinks. but the showing of the menu havent been implemented yet.

--- src/index.js
+++ src/index.js
@@ -4,3 +4,28 @@ import "./css/main.css";
 
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
+
+// Hamburger menu module
+window.addEventListener("load", (e) => {
+  setupHamburgerMenu();
+});
+
+function setupHamburgerMenu() {
+  let hamburgerMenuToggle = document.querySelector(".hamburger-menu-toggle");
+  // let navMenu = document.querySelector(".nav-main");
+
+  hamburgerMenuToggle.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (
+      hamburgerMenuToggle.classList.contains("hamburger-menu-toggle--cross")
+    ) {
+      // meaning hamburger already opened
+      hamburgerMenuToggle.classList.remove("hamburger-menu-toggle--cross");
+      // navMenu.classList.remove("nav-main--expanded");
+    } else {
+      // hamburger menu is collapsed
+      hamburgerMenuToggle.classList.add("hamburger-menu-toggle--cross");
+      // navMenu.classList.add("nav-main--expanded");
+    }
+  });
+}

300646d79fd556aa71db2396ef496b575f648064
Alvin <alvinleung2009@gmail.com>
2020-08-26 01:51:42 -0700

    Added form component (ver 0.1)

---
+++ site/static/registration-form/precache-manifest.bd2666ba71f6cf7bcdb6eda2dadf3ce9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41190f2116152919e71f2df9f9be39a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce7e86d9a478443861a5",
+    "url": "/static/css/main.284e790f.chunk.css"
+  },
+  {
+    "revision": "31039bb5cfdb17f5ddf4",
+    "url": "/static/js/2.718c39b1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.718c39b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce7e86d9a478443861a5",
+    "url": "/static/js/main.dafbf6db.chunk.js"
+  },
+  {
+    "revision": "dd95638b40464f7ab2ab",
+    "url": "/static/js/runtime-main.322f793d.js"
+  }
+]);
